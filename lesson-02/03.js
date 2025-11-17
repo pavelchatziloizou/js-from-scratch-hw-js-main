73261336b3ed9417d800a31615545afb448fbfc2
main <pavel.ulitionok@gmail.com>
2025-11-17 18:32:12 +0200

    second commit

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,21 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
+const score = prompt("Введите оценку:", 40); // тестовое значение, можно изменять
+let grade = false;
 // your code
+
+while (!grade) {
+    if (score >= 90 && score <= 100) {
+        grade = "A";
+    } else if (score >= 80 && score <= 89) {
+        grade = "B";
+    } else if (score >= 70 && score <= 79) {
+        grade = "C";
+    } else if (score >= 50 && score <= 69) {
+        grade = "D";
+    } else {
+        grade = "F";
+    }
+}
+console.log(grade);
