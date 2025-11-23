879e71a7dc1b7c5fcffd7a634410ba135c92bf87
main <pavel.ulitionok@gmail.com>
2025-11-23 11:40:21 +0200

    second commit

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,8 +1,9 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
 function calculateAverage(firstNumber, secondNumber) {
-    let expression = (firstNumber + secondNumber) / 2;
-    console.log(expression);
+    if ((expression = (firstNumber + secondNumber) / 2)) {
+        console.log(expression);
+    }
 }
 
 calculateAverage(10, 20);
